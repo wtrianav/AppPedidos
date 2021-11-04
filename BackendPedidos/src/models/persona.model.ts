637800054d4d80ccafd969c914471645752e72c9
Andŕes Triana <wtriana@outlook.com>
2021-11-04 12:15:14 -0500

    Versión 1.0.0 de la aplicación de pedidos

---
+++ BackendPedidos/src/models/persona.model.ts
@@ -0,0 +1,55 @@
+import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Pedido} from './pedido.model';
+
+@model()
+export class Persona extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  apellido: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  telefono: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  clave: string;
+
+  @hasMany(() => Pedido)
+  pedidos: Pedido[];
+
+  constructor(data?: Partial<Persona>) {
+    super(data);
+  }
+}
+
+export interface PersonaRelations {
+  // describe navigational properties here
+}
+
+export type PersonaWithRelations = Persona & PersonaRelations;
